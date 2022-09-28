67ef737ce3a9a247c57535d5015a68b3c34b0f90
Marcos Venegas <venegasalvarado@gmail.com>
2022-09-27 17:19:59 -0700

    draw init universe, create animation

--- proj0/Body.java
+++ proj0/Body.java
@@ -77,5 +77,7 @@ public class Body {
 		this.xxPos = this.xxPos + dt * this.xxVel;
 		this.yyPos = this.yyPos + dt * this.yyVel;
 	}
-
+	public void draw() {
+		StdDraw.picture(this.xxPos, this.yyPos, "images/"+this.imgFileName);
+	}
 }
--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,7 +1,8 @@
 public class NBody {
 	public static double readRadius(String str) {
 		In in = new In(str);
-		double radius = in.readDouble();
+		double radius;
+		radius = in.readDouble();
 		radius = in.readDouble();
 		return radius;
 	}
@@ -10,7 +11,7 @@ public class NBody {
 		/* Throwaways for first 2 nums */
 		in.readDouble();
 		in.readDouble();
-		Body[] bodies = new Body[5];
+		Body[] bodies = new Body[5]; // can this be generalized further???
 		for (int i = 0; i < bodies.length; i += 1) {
 			double xxPos = in.readDouble();
 			double yyPos = in.readDouble();
@@ -26,7 +27,32 @@ public class NBody {
 		double T = Double.parseDouble(args[0]);
 		double dt = Double.parseDouble(args[1]);
 		String filename = args[2];
-		readBodies(filename);
-		readRadius(filename);
+		double radius = readRadius(filename);
+		Body[] bodies = readBodies(filename);
+		StdDraw.setScale(-radius, radius);
+		StdDraw.picture(0, 0, "images/starfield.jpg");
+		for (Body b : bodies) {
+			b.draw();
+		}
+		double time = 0;
+		StdDraw.enableDoubleBuffering(); // might need to go somewhere else
+		while (time <= T) {
+			double[] xForces = new double[5]; // can this be generalized further???
+			double[] yForces = new double[5]; // can this be generalized further???
+			for (int i = 0; i < bodies.length; i += 1) {
+				xForces[i] = bodies[i].calcNetForceExertedByX(bodies);
+				yForces[i] = bodies[i].calcNetForceExertedByY(bodies);
+			}
+			for (int i = 0; i < bodies.length; i += 1) {
+				bodies[i].update(dt, xForces[i], yForces[i]);
+			}
+			StdDraw.picture(0, 0, "images/starfield.jpg");
+			for (Body b : bodies) {
+				b.draw();
+			}
+			StdDraw.show();
+			StdDraw.pause(10); //milliseconds, might bug
+			time += dt;
+		}
 	}
 }
