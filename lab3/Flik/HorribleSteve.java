043e2606a3084dac56c18979f265b3fb745c8283
Marcos Venegas <venegasalvarado@gmail.com>
2022-10-10 00:37:57 -0700

    run style checker on BuggyIntDList

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,12 +1,14 @@
 public class HorribleSteve {
     public static void main(String [] args) throws Exception {
         int i = 0;
-        for (int j = 0; i < 500; ++i, ++j) {
+        for (byte j = 0; i < 200; ++j) {
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
             }
+            System.out.println(i + " " + j);
+            i ++;
         }
-        System.out.println("i is " + i);
+        //System.out.println("i is " + i);
     }
 }
