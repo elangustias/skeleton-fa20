d7aa65172eb3f6a39454082dc3d2ae60581c74d9
Marcos Venegas <venegasalvarado@gmail.com>
2022-10-24 20:09:28 -0700

    create AListFloorSet, test RedBlackFloorSet to show bug in method

--- lab5/TestRedBlackFloorSet.java
+++ lab5/TestRedBlackFloorSet.java
@@ -7,6 +7,18 @@ import static org.junit.Assert.assertEquals;
 public class TestRedBlackFloorSet {
     @Test
     public void randomizedTest() {
-       // TODO: YOUR CODE HERE
+        AListFloorSet aList = new AListFloorSet();
+        RedBlackFloorSet rbList = new RedBlackFloorSet();
+        for (int i = 0; i < 1000000; i++) {
+            Double curr = StdRandom.uniform(-5000.00, 5000.00);
+            aList.add(curr);
+            rbList.add(curr);
+        }
+        for (int i = 0; i < 100000; i++) {
+            Double floor = StdRandom.uniform(-5000.00, 5000.00);
+            Double expected = aList.floor(floor);
+            Double actual = rbList.floor(floor);
+            assertEquals(expected, actual, 0.1);
+        }
     }
 }
