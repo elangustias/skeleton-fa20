44bfb2667a5f6453f6a649279780f98037d458b9
Marcos Venegas <venegasalvarado@gmail.com>
2022-11-22 00:20:41 -0800

    added comment for first attempt at solving

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,9 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-
+    /* How can i use quick union to represent an N by N grid? Maybe use a calculation that uses the grid positions:
+       pos [0, 4] (assuming we're in bounds) will be represented by the number 5 bc pos[0] * N = 0, then we add
+       that to pos[1] + 1 = 5.
+       pos [1, 3] is pos[1] * N (let's say N is 5) = 5, then we add pos[1] + 1 = 9
+     */
 }
