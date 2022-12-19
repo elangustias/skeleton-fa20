153d400799e4a158759abd6961541ed567a5d904
Marcos Venegas <venegasalvarado@gmail.com>
2022-12-18 19:28:06 -0800

    Finish and test Percolation stats

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,28 +1,52 @@
 package hw2;
 
 import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
-    int[] percolationNums;
+    private int[] percolationNums;
+    private double meanNum = -1.0;
+    private double stddevNum = -1.0;
     public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
         percolationNums = new int[T];
         int[] grid = new int[N*N];
-        for (int i = 0; i < grid.length; i++) {
-            grid[i] = i;
+        for (int i = 0; i < N*N; i++) {
+            grid[i] = i + 1;
         }
         for (int i = 0; i < T; i++) {
-            int index = 0;
             int percolationCount = 0;
             Percolation curr = pf.make(N);
             StdRandom.shuffle(grid);
+            int index = 0;
             while (!curr.percolates()) {
-                int row = grid[index] / N;
-                int col = grid[index] % N;
+                int row = (grid[index] - 1) / N;
+                int col = (grid[index] - 1) % N;
                 curr.open(row, col);
                 percolationCount++;
+                index++;
             }
-            percolationNums[index] = percolationCount;
-            index++;
+            percolationNums[i] = percolationCount;
         }
+    }
+    public double mean() {
+        if (meanNum < 0) {
+            meanNum = StdStats.mean(percolationNums);
+        }
+        return meanNum;
+    }
+    public double stddev() {
+        if (stddevNum < 0) {
+            stddevNum = StdStats.stddev(percolationNums);
+        }
+        return stddevNum;
+    }
+    public double confidenceLow() {
+        return mean() - (1.96 * stddev() / Math.sqrt(percolationNums.length));
+    }
+    public double confidenceHigh() {
+        return mean() + (1.96 * stddev() / Math.sqrt(percolationNums.length));
     }
 }
