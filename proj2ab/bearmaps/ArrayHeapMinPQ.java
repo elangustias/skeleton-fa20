753b4a3681adbd5713b1034e0c177e42a3852af1
Marcos Venegas <venegasalvarado@gmail.com>
2023-01-28 10:56:44 -0800

    Finish ArrayHeapMinPQ, no testing yet

--- proj2ab/bearmaps/ArrayHeapMinPQ.java
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -1,11 +1,11 @@
 package bearmaps;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
-    int size = 0;
+    private int size = 0;
     Node[] minHeap = new ArrayHeapMinPQ.Node[10];
-    HashSet<Node> minSet = new HashSet<>();
+    private HashMap<T, Node> nodeMap;
     private class Node {
         T item;
         double priority;
@@ -19,50 +19,38 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         public int hashCode() {
             return item.hashCode();
         }
-        @Override
-        public boolean equals(Object o) {
-            if (o == null || (o.getClass() != this.getClass() || o.getClass() != this.item.getClass())) {
-                return false;
-            }
-            // This might always return true bc all objects are of class object
-            // ... and java doesn't know if the object is actually an item or node
-            if (o.getClass() == this.getClass()) {
-                return ((Node) o).item.equals(this.item);
-            } else { // If Object o is of class T, which it normally should be
-                return o.equals(this.item);
-            }
-        }
     }
     @Override
     public void add(T item, double priority) {
-        Node n = new Node(item, priority);
-        if (!(minSet.contains(n))) {
-            minSet.add(n);
-            minHeap[size] = n;
-            swim(size);
-            size++;
-            minHeap = resize();
+        if (nodeMap.containsKey(item)) {
+            throw new IllegalArgumentException("Item already exists");
         }
+        Node n = new Node(item, priority);
+        nodeMap.put(item, n);
+        minHeap[size] = n;
+        swim(size);
+        size++;
+        resize();
+
     }
     private void swim(int index) {
-        int currIndex = index;
-        while (currIndex != 0) {
-            int parentIndex = (currIndex - 1) / 2;
-            if (minHeap[currIndex].priority < minHeap[parentIndex].priority) {
+        while (index != 0) {
+            int parentIndex = (index - 1) / 2;
+            if (minHeap[index].priority < minHeap[parentIndex].priority) {
                 Node pCopy = minHeap[parentIndex];
-                minHeap[parentIndex] = minHeap[currIndex];
-                minHeap[currIndex] = pCopy;
-                minHeap[currIndex].index = currIndex;
-                currIndex = parentIndex;
+                minHeap[parentIndex] = minHeap[index];
+                minHeap[index] = pCopy;
+                minHeap[index].index = index; // Updates the demoted Node's index before reassigning index value
+                index = parentIndex;
             } else {
-                minHeap[currIndex].index = currIndex;
+                minHeap[index].index = index;
                 break;
             }
         }
     }
     @Override
     public boolean contains(T item) {
-        return minSet.contains(item);
+        return nodeMap.containsKey(item);
     }
     @Override
     public T getSmallest() {
@@ -74,34 +62,35 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
             throw new NoSuchElementException("Priority Queue is empty");
         }
         T smallest = minHeap[0].item;
-        minSet.remove(smallest);
-        minHeap[0] = minHeap[size];
-        minHeap[size] = null;
+        nodeMap.remove(smallest);
+        minHeap[0] = minHeap[size-1];
+        minHeap[size-1] = null;
         sink(0);
         size--;
-        minHeap = resize();
+        resize();
         return smallest;
     }
     private void sink(int index) {
-        int smallChild = smallestChild(index);
-        while (smallChild > 0) {
-            if (minHeap[index].priority < minHeap[smallChild].priority) {
+        int childIndex = smallestChild(index);
+        while (childIndex > 0) {
+            if (minHeap[index].priority <= minHeap[childIndex].priority) {
                 minHeap[index].index = index;
                 break;
             } else {
                 Node pCopy = minHeap[index];
-                minHeap[index] = minHeap[smallChild];
-                minHeap[index].index = index;
-                minHeap[smallChild] = pCopy;
-                index = smallChild;
-                smallChild = smallestChild(index);
+                minHeap[index] = minHeap[childIndex];
+                minHeap[index].index = index; // Updates the promoted Node's index before reassigning index value
+                minHeap[childIndex] = pCopy;
+                index = childIndex;
+                childIndex = smallestChild(index);
             }
         }
     }
     private int smallestChild(int index) {
         int left = index*2+1;
         int right = index*2+2;
-        if (minHeap[left] == null && minHeap[right] == null) {
+        boolean outOfBounds = right >= size;
+        if (outOfBounds || (minHeap[left] == null && minHeap[right] == null)) {
             return -1;
         } else if (minHeap[right] == null || minHeap[left].priority < minHeap[right].priority) {
             return left;
@@ -113,13 +102,25 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
     public int size() {
         return size;
     }
-    /* I think I'll have to build a TreeMap instead of the HashSet bc for
-     * changePriority I'm going to have to be able to retrieve the index from the
-     * utility set, which is useless bc sets don't serve that purpose.
-     */
     @Override
     public void changePriority(T item, double priority) {
-        if (minSet.contains(item))
+        Node n = nodeMap.get(item);
+        if (n == null) {
+            throw new NoSuchElementException("Item doesn't exist");
+        }
+        n.priority = priority;
+        sink(n.index);
+        swim(n.index);
     }
-
+    private void resize() {
+        Double fill = Double.valueOf(size) / minHeap.length;
+        if (0.25 < fill && fill < 1.00) {
+            return;
+        }
+        Node[] newHeap = (Node []) new Object[size*2];
+        for (int i = 0; i < size; i++) {
+            newHeap[i] = minHeap[i];
+        }
+        minHeap = newHeap;
+    }
 }
